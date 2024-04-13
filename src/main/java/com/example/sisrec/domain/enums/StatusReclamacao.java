b752500d1088d18eb38515e2e09beef3cce57f5a
Antonybsb <artbsb1@gmail.com>
2024-04-13 11:12:49 -0300

    criacao enum status reclamacao

--- src/main/java/com/example/sisrec/domain/enums/StatusReclamacao.java
+++ src/main/java/com/example/sisrec/domain/enums/StatusReclamacao.java
@@ -1,7 +1,34 @@
 package com.example.sisrec.domain.enums;
 
 public enum StatusReclamacao {
-    ABERTO,
-    ANDAMENTO,
-    CONCLUIDA
+    ABERTO(0, "ABERTO"), ANDAMENTO(1, "ANDAMENTO"), CONCLUIDA(2, "CONCLUIDA");
+
+    private Integer codigo;
+    private String descricao;
+
+    StatusReclamacao(Integer codigo, String descricao) {
+        this.codigo = codigo;
+        this.descricao = descricao;
+    }
+
+    public Integer getCodigo() {
+        return codigo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public static StatusReclamacao toEnum(Integer cod) {
+        if (cod == null) {
+            return null;
+        }
+            for(StatusReclamacao x : StatusReclamacao.values()) {
+                if (cod.equals(x.getCodigo())) {
+                    return x;
+                }
+            }
+            throw new IllegalArgumentException("Reclamação iválida");
+        }
+    
 }
